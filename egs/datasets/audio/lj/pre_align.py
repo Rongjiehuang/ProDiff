523501c4d69c45f04a8434f68b67978b559018d9
Inpyo Lee <mynameispyo@gmail.com>
2022-09-24 13:26:02 -0400

    fix pre align

--- egs/datasets/audio/lj/pre_align.py
+++ egs/datasets/audio/lj/pre_align.py
@@ -6,7 +6,9 @@ class LJPreAlign(BasePreprocessor):
         for l in open(f'{self.raw_data_dir}/metadata.csv').readlines():
             item_name, _, txt = l.strip().split("|")
             wav_fn = f"{self.raw_data_dir}/wavs/{item_name}.wav"
-            yield item_name, wav_fn, txt, 'SPK1'
+            # yield item_name, wav_fn, txt, 'SPK1'
+            yield {'item_name': item_name, 'wav_fn': wav_fn, 'txt': txt, 'spk_name': 'SPK1', 'txt_loader': None}
+
 
 
 if __name__ == "__main__":
